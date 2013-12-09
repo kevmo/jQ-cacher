f47b36a47bfb3987b33b74d3141176bcc3f43850
kevmo <k.james.moore@gmail.com>
2013-12-09 08:37:03 -0700

    added plugin code, working on demo page

--- scripts/jquery.loader.js
+++ scripts/jquery.loader.js
@@ -9,3 +9,36 @@
     }
   };
 })(jQuery);
+
+  //helper: create img el, append to el identified as
+  //imgContainer in the config object passed to fn
+  function simpleLoad(config){
+    for (var i = 1; i < config.imgTotal + 1; i++){
+      $('<img>').attr({
+        id: "image" + i,
+        src: config.imgDir + x + config.imgFormat,
+        title: "Image" + x
+      }).appendTo("#" + config.imgContainer).css({display:"none"});
+    }
+  }
+
+  //helper: takes config obj, fileNames array
+  function complexLoad(config, fileNames){
+    for (var i = 0; i < fileNames.length; i++){
+      $('<img>').attr({
+        id: fileNames[i],
+        src: config.imgDir + fileNames[i] + config.imgFormat,
+        title: fileNames[i]
+      }).appendTo("#" + config.imgContainer).css({display:"none"});
+    }
+  }
+
+  //the plugin, 2 optional arguments
+  $.fn.jLoader = function (configOptional, fileNames){
+    var config = $.extend({}, $.jLoader.defaults, configOptional);
+    config.imgContainer = this.attr("id");
+    (config.simpleFileNames == true) ? simpleLoad(config) : complexLoad (config, fileNames);
+    return this;
+  }
+
+})(jQuery);
